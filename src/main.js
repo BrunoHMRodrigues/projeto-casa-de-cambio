deb7f7ca9d22dea8e3cabb18ee3453b63dd1748c
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-12 13:17:27 -0300

    Resolvendo problemas e alterações de estilo

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const currency = document.getElementById('input-currency')
 const btnSearch = document.getElementById('btn-search');
 const ul = document.getElementById('ul-currency');
-
+const header = document.getElementById('header-result');
+let allLi;
 // const apiSearch = `https://api.exchangerate.host/convert?from=${getCur}&to=`
 
 btnSearch.addEventListener('click', async () =>{
@@ -23,13 +24,14 @@ btnSearch.addEventListener('click', async () =>{
             throw new Error('Moeda informada não foi encontrada no banco de dados.');
         }
         const currencies = Object.entries(result.rates);
-        console.log(currencies);
-        currencies.forEach(element => {
+        currencies.forEach((element) => {
             const li = document.createElement('li');
             li.classList.add('list-currency');
             li.innerHTML = `<div class="container-coin"><i class="bi bi-coin"></i><p class="coin">${element[0]}</p></div><p class="conversion">${element[1]}</p>`;
             ul.appendChild(li);
         });
+        allLi = document.querySelectorAll('.list-currency');
+        header.innerText = `Valores referentes a 1 ${getCur.toUpperCase()}`
         ul.style.overflowY = 'scroll';
     } catch(error) {
         Swal.fire({
@@ -38,4 +40,29 @@ btnSearch.addEventListener('click', async () =>{
             text: error.message,
           })
     }    
+})
+
+ul.addEventListener('click', (event) => {
+    const target = event.target;
+    const targetClass = target.className;
+    if (targetClass === 'list-currency') {
+        target.classList.add('selected');
+    }
+    if (targetClass === 'list-currency selected') {
+        target.classList.remove('selected');
+    }
+
+    if (event.path[1].className === 'list-currency selected') {
+        event.path[1].className = 'list-currency';
+    } else if (targetClass === 'conversion') {
+        event.path[1].classList.add('selected');
+    }    
+
+    if (event.path[2].className === 'list-currency selected') {
+        event.path[2].classList.remove('selected');
+    } else if (targetClass === 'coin' || targetClass === 'bi bi-coin') {
+        event.path[2].classList.add('selected');
+    }    
+
+    // console.log(allLi);
 })
