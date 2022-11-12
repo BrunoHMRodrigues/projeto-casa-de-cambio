65a4b4c98d2de97549050ed33ff5e1b6c7b6fdb1
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-12 11:25:51 -0300

    Realizado lógica de erro para quando nenhuma moeda for informada ou a moeda não for encontrada

--- src/main.js
+++ src/main.js
@@ -8,18 +8,34 @@ const ul = document.getElementById('ul-currency');
 // const apiSearch = `https://api.exchangerate.host/convert?from=${getCur}&to=`
 
 btnSearch.addEventListener('click', async () =>{
-    const getCur = currency.value;
-    const API = `https://api.exchangerate.host/latest?base=${getCur}`;
-    const currencies = await fetch(API)
-    .then(response => response.json())
-    .then(data => Object.entries(data.rates));
-
-    console.log(currencies);
-
-    currencies.forEach(element => {
-        const li = document.createElement('li');
-        li.classList.add('list-currency');
-        li.innerHTML = `<div class="container-coin"><i class="bi bi-coin"></i><p class="coin">${element[0]}</p></div><p class="conversion">${element[1]}</p>`;
-        ul.appendChild(li);
-    });
+    ul.innerHTML = '';
+    try {
+        if (currency.value === '') {
+            throw new Error('Necessário informar uma moeda.');
+        }
+        const getCur = currency.value;
+        const API = `https://api.exchangerate.host/latest?base=${getCur}`;
+    
+        const result = await fetch(API)
+            .then(response => response.json())
+            .then(data => data)
+        if (result.base !== getCur.toUpperCase()) {
+            throw new Error('Moeda informada não foi encontrada no banco de dados.');
+        }
+        const currencies = Object.entries(result.rates);
+        console.log(currencies);
+        currencies.forEach(element => {
+            const li = document.createElement('li');
+            li.classList.add('list-currency');
+            li.innerHTML = `<div class="container-coin"><i class="bi bi-coin"></i><p class="coin">${element[0]}</p></div><p class="conversion">${element[1]}</p>`;
+            ul.appendChild(li);
+        });
+        ul.style.overflowY = 'scroll';
+    } catch(error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error.message,
+          })
+    }    
 })
